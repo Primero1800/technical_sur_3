c8eb2a5484d436301538fee404fc8488e9b1e99c
Primero1800 <Primero@inbox.ru>
2025-03-11 10:34:51 +0300

    feat(app) DB_NAME_TEST added

--- app1/api/v1/__init__.py
+++ app1/api/v1/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
 from app1.core.settings import settings
-from app1.api.v1.users.views import router as users_router
+# from app1.api.v1.users.views import router as users_router
 
 router = APIRouter()
 
-router.include_router(
-    users_router,
-    tags=[settings.tags.USERS_TAG,],
-)
+# router.include_router(
+#     users_router,
+#     tags=[settings.tags.USERS_TAG,],
+# )

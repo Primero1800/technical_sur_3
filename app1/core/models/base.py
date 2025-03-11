6cc3bdb7c31d21b5d35ff0a6f76d5e21a35677a0
Primero1800 <Primero@inbox.ru>
2025-03-11 11:47:35 +0300

    feat(users) User model created

--- app1/core/models/base.py
+++ app1/core/models/base.py
@@ -16,8 +16,6 @@ class Base(DeclarativeBase):
         return DBConfigurer.utils.camel2snake(cls.__name__)
         # return '_'.join([settings.db.DB_TABLE_PREFIX, cls.__name__.lower()])
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-
     def to_dict(self):
         result = {}
         for column in self.__table__.columns:

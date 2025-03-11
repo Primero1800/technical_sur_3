33a5f665ebf0822afced59a6ca28737a351e4984
Primero1800 <Primero@inbox.ru>
2025-03-11 13:43:19 +0300

    feat(AccessToken) strategy added

---
+++ app1/api/dependencies/strategy.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+
+from app1.core.settings import settings
+
+if TYPE_CHECKING:
+    from app1.core.models import AccessToken
+
+
+def get_database_strategy(
+    access_token_db: AccessTokenDatabase["AccessToken"] = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        access_token_db,
+        lifetime_seconds=settings.access_token.ACCESS_TOKEN_LIFETIME,
+    )

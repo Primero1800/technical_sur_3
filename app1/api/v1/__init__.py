60c81ca6097cae43ffb47d2367bc1c78ffe22f38
Primero1800 <Primero@inbox.ru>
2025-03-11 16:11:39 +0300

    feat(api) auth router added

--- app1/api/v1/__init__.py
+++ app1/api/v1/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
 from app1.core.settings import settings
-# from app1.api.v1.users.views import router as users_router
+from app1.api.v1.auth import router as auth_router
 
 router = APIRouter()
 
-# router.include_router(
-#     users_router,
-#     tags=[settings.tags.USERS_TAG,],
-# )
+router.include_router(
+    auth_router,
+    prefix=settings.tags.AUTH_PREFIX,
+    tags=[settings.tags.AUTH_TAG,],
+)

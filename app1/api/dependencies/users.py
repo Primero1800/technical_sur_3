e8bf89f31cf0e807022cf5341274c115a2b542c8
Primero1800 <Primero@inbox.ru>
2025-03-11 15:41:19 +0300

    feat(api) schemas replaced, fastapi_users dependency added

--- app1/api/dependencies/users.py
+++ app1/api/dependencies/users.py
@@ -1,7 +1,11 @@
 from typing import TYPE_CHECKING
 
 from fastapi import Depends
+from fastapi_users import FastAPIUsers
+from sqlalchemy import Integer
 
+from .user_manager import get_user_manager
+from .backend import authentication_backend
 from app1.core.models import User
 from app1.core.config import DBConfigurer
 
@@ -13,3 +17,9 @@ async def get_user_db(
         session: "AsyncSession" = Depends(DBConfigurer.session_getter)
 ):
     yield User.get_db(session=session)
+
+
+fastapi_users = FastAPIUsers[User, Integer](
+    get_user_manager,
+    [authentication_backend],
+)

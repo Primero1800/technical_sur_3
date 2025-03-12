0c9936da9d10e44a2a12ec6aa02c10bbccf11ae9
Primero1800 <Primero@inbox.ru>
2025-03-12 11:09:45 +0300

    feat(api) bearer dependency added

--- app1/api/v1/__init__.py
+++ app1/api/v1/__init__.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from app1.core.settings import settings
 from app1.api.v1.auth import router as auth_router
 from app1.api.v1.users import router as users_router
 
-router = APIRouter()
+http_bearer = HTTPBearer(
+    auto_error=False
+)
+
+router = APIRouter(
+    dependencies=[Depends(http_bearer)]
+)
 
 router.include_router(
     auth_router,

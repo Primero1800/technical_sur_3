e61b9fdd080dabce56ec3cb6f4b90c2626dcf501
Primero1800 <Primero@inbox.ru>
2025-03-13 12:07:00 +0300

    feat(api) column displayed_name added to User model and db

--- app1/core/models/user.py
+++ app1/core/models/user.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 from fastapi_users.db import (
     SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase,
 )
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app1.core.models.base import Base
 from app1.core.models.mixins import IDIntPkMixin
@@ -12,13 +14,18 @@ if TYPE_CHECKING:
 
 
 class User(Base, IDIntPkMixin, SQLAlchemyBaseUserTable[int]):
+    displayed_name: Mapped[str] = mapped_column(
+        default='Incognito', server_default='Noname',
+        nullable=False, type_=String(50),
+        name='Displayed user name',
+    )
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session,  cls)
 
     def __str__(self):
-        return f"{self.__class__.__name__}(id={self.id}, email={self.email})"
+        return f"{self.__class__.__name__}(id={self.id}, email={self.email}, displayed_name={self.displayed_name})"
 
     def __repr__(self):
         return str(self)

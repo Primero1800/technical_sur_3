cbe84000910342d4bdf8f15e492e27e30e9e6421
Primero1800 <Primero@inbox.ru>
2025-03-17 11:11:39 +0300

    refactoring(webhook) unused import delete

--- app1/core/models/access_token.py
+++ app1/core/models/access_token.py
@@ -1,17 +1,10 @@
-from typing import TYPE_CHECKING
-
 from fastapi_users_db_sqlalchemy.access_token import (
-    SQLAlchemyAccessTokenDatabase,
     SQLAlchemyBaseAccessTokenTable,
 )
 from sqlalchemy import ForeignKey, Integer
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app1.core.models import Base, User
-
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[int]):

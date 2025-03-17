705669cbb2968b84eff5d6ecc3e67e6f95958ca0
Primero1800 <Primero@inbox.ru>
2025-03-17 13:40:07 +0300

    feat(scripts) delete_expired_tokens added

--- app1/scripts/delete_expired_tokens.py
+++ app1/scripts/delete_expired_tokens.py
@@ -1,32 +1,42 @@
 import asyncio
-import contextlib
-from logging import Logger
+import logging
+from datetime import timedelta
 from typing import Sequence
-from sqlalchemy import select, Result
+from sqlalchemy import select, Result, func, delete
 
-
+from app1.core.config import DBConfigurer
 from app1.core.models import AccessToken
 
 
+app_logger = logging.getLogger(__name__)
+
+
+# PYTHONPATH=.. python scripts/delete_expired_tokens.py
 async def delete_expired_tokens_from_db(
-        logger: Logger = None,
+        logger: logging.Logger = app_logger,
 ) -> Sequence[AccessToken]:
-    # from app1.core.auth.access_token import get_access_token_db
-    from app1.core.config import DBConfigurer
+
+    logger.info("SCRIPT delete_expired_tokens_from_db STARTED.")
+    from app1.core.settings import settings
 
     async with DBConfigurer.Session() as session:
 
-        # get_access_token_db_context = contextlib.asynccontextmanager(get_access_token_db)
-        # async with get_access_token_db_context(session) as access_token_db:
+        expired_frontier = func.now() - timedelta(
+            seconds=settings.access_token.ACCESS_TOKEN_LIFETIME
+        )
+        stmt = select(AccessToken).where(AccessToken.created_at < expired_frontier)
+        result: Result = await session.execute(stmt)
+        result_to_return = result.scalars().all()
+        logger.info(f"Expired tokens to delete: {len(result_to_return)}")
 
-            stmt = select(AccessToken).order_by(AccessToken.user_id)
-            result: Result = await session.execute(stmt)
+        stmt = delete(AccessToken).where(AccessToken.created_at < expired_frontier)
+        await session.execute(stmt)
+        logger.info(f"Expired tokens to deleted")
 
-            result_to_return = result.scalars().all()
-            print("QQQ", result_to_return)
-            return result_to_return
+        return result_to_return
 
 
 if __name__ == "__main__":
-    print("Scrypt starting")
-    asyncio.run(delete_expired_tokens_from_db())
+    result = asyncio.run(delete_expired_tokens_from_db())
+
+    print(result)

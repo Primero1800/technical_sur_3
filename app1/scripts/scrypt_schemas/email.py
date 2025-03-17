16279e0a21f419364e23c3ff18020297882f0eb0
Primero1800 <Primero@inbox.ru>
2025-03-17 15:55:30 +0300

    feat(app) mail_tester endpoint added

--- app1/scripts/scrypt_schemas/email.py
+++ app1/scripts/scrypt_schemas/email.py
@@ -4,6 +4,6 @@ from pydantic import BaseModel
 
 
 class MailBody(BaseModel):
-    to: List[str] = ['admin@primero1800.store']
+    to: List[str] = ['admin@primero1800.store', 'primero@inbox.ru', ]
     subject: str = 'Test Message'
     body: str = "Hello! You are reading test message"

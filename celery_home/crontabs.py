ad01542e7e1bdde62fecfff219a5e5ffc50122fd
Primero1800 <Primero@inbox.ru>
2025-03-22 14:16:46 +0300

    feat(app) celery beat preparing

--- celery_home/crontabs.py
+++ celery_home/crontabs.py
@@ -6,4 +6,4 @@ class Crontabs:
     every_2_minutes: crontab = crontab(minute='*/2')           # Запуск каждые 2 минуты
     every_10_minutes: crontab = crontab(minute='*/10')       # Запуск каждые 10 минут
     every_day: crontab = crontab(minute='0', hour='0')      # Запуск каждый день в 00:00
-    every_hour: crontab = crontab(hour='*')                         # Запуск каждый час
+    every_hour: crontab = crontab(minute='0')                         # Запуск каждый час

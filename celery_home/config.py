bc9d7e7ed74712e56e89f4544fc60bdc0c9851d9
Primero1800 <Primero@inbox.ru>
2025-03-24 11:13:15 +0300

    refactoring(app) celery configuration remade

--- celery_home/config.py
+++ celery_home/config.py
@@ -1,5 +1,6 @@
 from celery import Celery
 
+from celery_home.crontabs import Crontabs
 
 # command
 #  celery -A celery_home.config.app_celery worker --loglevel=info | celery -A celery_home.config.app_celery flower | celery -A celery_home.config.app_celery beat  --loglevel=info
@@ -14,3 +15,30 @@ app_celery.autodiscover_tasks(
 )
 
 
+beat_schedule = {
+    # 'run-every-minute': {
+    #     'task': 'task_beat_test_every_minute',
+    #     'schedule': Crontabs.every_minute,
+    #     'args': (100, )
+    # },
+    'run-every-2-minutes': {
+        'task': 'task_beat_test_every_minute',
+        'schedule': Crontabs.every_2_minutes,
+        'args': (200,)
+    },
+    'run-every-hour': {
+        'task': 'task_beat_test_every_minute',
+        'schedule': Crontabs.every_hour,
+        'args': (6000,)
+    },
+    # 'run-every-10-minutes': {
+    #     'task': 'task_beat_test_every_minute',
+    #     'schedule': Crontabs.every_10_minutes,
+    #     'args': (1000,)
+    # },
+    # 'run-every-day': {
+    #     'task': None,
+    #     'schedule': Crontabs.every_day,
+    #     'args': (10, 20)
+    # },
+}
--- celery_home/file_config.py
+++ celery_home/file_config.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 
-from celery_home.crontabs import Crontabs
 from celery_home.settings import settings
 
 broker_url = settings.celery.CELERY_BROKER_URL
@@ -16,32 +15,3 @@ timezone = settings.celery.APP_TIMEZONE
 
 result_expires = timedelta(days=settings.celery.CELERY_RESULT_EXPIRES_DAYS)
 # result_expires = timedelta(hours=settings.celery.CELERY_RESULT_EXPIRES_HOURS)
-
-
-beat_schedule = {
-    # 'run-every-minute': {
-    #     'task': 'task_beat_test_every_minute',
-    #     'schedule': Crontabs.every_minute,
-    #     'args': (100, )
-    # },
-    'run-every-2-minutes': {
-        'task': 'task_beat_test_every_minute',
-        'schedule': Crontabs.every_2_minutes,
-        'args': (200,)
-    },
-    'run-every-hour': {
-        'task': 'task_beat_test_every_minute',
-        'schedule': Crontabs.every_hour,
-        'args': (6000,)
-    },
-    # 'run-every-10-minutes': {
-    #     'task': 'task_beat_test_every_minute',
-    #     'schedule': Crontabs.every_10_minutes,
-    #     'args': (1000,)
-    # },
-    # 'run-every-day': {
-    #     'task': None,
-    #     'schedule': Crontabs.every_day,
-    #     'args': (10, 20)
-    # },
-}

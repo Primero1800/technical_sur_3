8d41f584a1b6d05f710650b3741bda7d37771ae5
Primero1800 <Primero@inbox.ru>
2025-03-25 12:19:51 +0300

    refactoring(app) base

--- app1/core/models/base.py
+++ app1/core/models/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 from app1.core.config.db_config import DBConfigurer
 from app1.core.settings import settings
 

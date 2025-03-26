51e21cf94b63e3077cb62c8bc6ba5fe657d850a3
Primero1800 <Primero@inbox.ru>
2025-03-26 17:38:46 +0300

    refactoring(app) schemas reorganized

--- app1/api/v1/store/brands/schemas.py
+++ app1/api/v1/store/brands/schemas.py
@@ -1,21 +1,25 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from pydantic import BaseModel, Field, ConfigDict
+
+base_title_field = Annotated[str, Field(
+        min_length=3, max_length=100,
+        title="Brand's title",
+        description="Brand's title that used in application"
+    )]
+
+base_description_field = Annotated[str, Field(
+        max_length=500,
+        title="Brand's description",
+        description="Brand's description that used in application"
+    )]
 
 
 class BaseBrand(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    title: Annotated[str, Field(
-        min_length=3, max_length=100,
-        title="Brand's title",
-        description="Brand's title that used in application"
-    )]
-    description: Annotated[str, Field(
-        max_length=500,
-        title="Brand's description",
-        description="Brand's description that used in application"
-    )]
+    title: base_title_field
+    description: base_description_field
 
 
 class BrandCreate(BaseBrand):
@@ -29,3 +33,8 @@ class BrandRead(BaseBrand):
 
 class BrandUpdate(BaseBrand):
     pass
+
+
+class BrandPartialUpdate(BaseBrand):
+    title: Optional[base_title_field]
+    description: Optional[base_description_field]

0490ce32c0744ed6ab87fcd6393972eb80074bc3
Primero1800 <Primero@inbox.ru>
2025-03-28 11:38:27 +0300

    feat(app) rubric dependencies added

--- app1/api/v1/store/rubrics/dependencies.py
+++ app1/api/v1/store/rubrics/dependencies.py
@@ -2,19 +2,19 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app1.core.config import DBConfigurer
-from app1.core.models import Brand
+from app1.core.models import Rubric
 
 
 async def get_one_simple(
-    brand_id: int,
+    rubric_id: int,
     session: AsyncSession = Depends(DBConfigurer.session_getter)
-) -> Brand:
+) -> Rubric:
 
-    brand: Brand | None = await session.get(Brand, brand_id)
+    rubric: Rubric | None = await session.get(Rubric, rubric_id)
 
-    if not brand:
+    if not rubric:
         raise HTTPException(
-            detail=f"Brand with id={brand_id} not found",
+            detail=f"Rubric with id={rubric_id} not found",
             status_code=status.HTTP_404_NOT_FOUND
         )
-    return brand
+    return rubric

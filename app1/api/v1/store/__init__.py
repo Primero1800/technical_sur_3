b30a5ce9f6612a8c9e48fc7dc2b79777104092c7
Primero1800 <Primero@inbox.ru>
2025-03-28 10:13:22 +0300

    feat(app) rubrics router added

--- app1/api/v1/store/__init__.py
+++ app1/api/v1/store/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app1.core.settings import settings
 from .brands import router as brands_router
+from .rubrics import router as rubrics_router
 
 
 router = APIRouter()
@@ -10,4 +11,11 @@ router.include_router(
     brands_router,
     prefix=settings.tags.BRANDS_PREFIX,
     tags=[settings.tags.BRANDS_TAG,],
+)
+
+
+router.include_router(
+    rubrics_router,
+    prefix=settings.tags.RUBRICS_PREFIX,
+    tags=[settings.tags.RUBRICS_TAG,],
 )

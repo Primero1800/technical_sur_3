54c1675d99c34177770562925777cba085b4c0b8
Primero1800 <Primero@inbox.ru>
2025-03-30 16:15:32 +0300

    refactoring(app) dependenci get_one_complex added

--- app1/api/v1/store/products/dependencies.py
+++ app1/api/v1/store/products/dependencies.py
@@ -1,8 +1,10 @@
 from fastapi import Depends, HTTPException, status
+from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from app1.core.config import DBConfigurer
-from app1.core.models import Product
+from app1.core.models import Product, Rubric, Brand
 
 CLASS = "Product"
 
@@ -18,5 +20,27 @@ async def get_one_simple(
         raise HTTPException(
             detail=f"{CLASS} with id={id} not found",
             status_code=status.HTTP_404_NOT_FOUND
+        )
+    return orm_model
+
+
+async def get_one_complex(
+    id: int = None,
+    session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> Product:
+
+    stmt_select = select(Product).where(Product.id == id)
+    stmt = stmt_select.options(
+        joinedload(Product.images),
+        joinedload(Product.rubrics).joinedload(Rubric.image),
+        joinedload(Product.brand).joinedload(Brand.image),
+    )
+    result: Result = await session.execute(stmt)
+    orm_model: Product | None = result.unique().scalar()
+
+    if not orm_model:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"{CLASS} with id={id} not found",
         )
     return orm_model

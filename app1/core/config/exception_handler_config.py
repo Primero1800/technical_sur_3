fa73b09d8967561384f1cfeded50c6a0960d0b87
Primero1800 <Primero@inbox.ru>
2025-03-31 17:05:49 +0300

    feat(app) CustomException changed

--- app1/core/config/exception_handler_config.py
+++ app1/core/config/exception_handler_config.py
@@ -48,15 +48,18 @@ class ExceptionHandlerConfigurer:
         @app.exception_handler(IntegrityError)
         async def integrity_error_exception_handler(request, exc: IntegrityError):
             logger.error("Handled by ExceptionHandler", exc_info=exc)
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"{errors.get_message(exc)} - HHHHHHHHHHHHHHHHHHHHAAAAAAAAAAAAAANNNNNNNNNNNNDDDDDDDDDDLLLLLLLLLEEEEEEEEER",
+            return ORJSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                    "message": "Handled by ExceptionHandler",
+                    "detail": f"Error while operation with database",
+                }
             )
 
         @app.exception_handler(CustomException)
         async def integrity_error_exception_handler(request, exc: CustomException):
             return ORJSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
+                status_code=exc.status_code,
                 content={
                     "message": "Handled by ExceptionHandler",
                     "detail": f"{exc.msg} (catched by exception_handler)",
--- app1/exceptions.py
+++ app1/exceptions.py
@@ -1,3 +1,10 @@
+from fastapi import status
+
 class CustomException(Exception):
-    def __init__(self, msg: str):
+    def __init__(
+            self,
+            msg: str,
+            status_code: status = status.HTTP_400_BAD_REQUEST,
+    ):
         self.msg = msg
+        self.status_code = status_code

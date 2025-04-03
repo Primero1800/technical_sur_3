5d2099e87cf5410c1d99014cc4b7d573b75c0825
Primero1800 <Primero@inbox.ru>
2025-04-03 16:53:14 +0300

    feat(app) Session model added

--- app1/core/models/__init__.py
+++ app1/core/models/__init__.py
@@ -10,13 +10,17 @@ __all__ = (
     "Rubric",
     "RubricImage",
     "RubricProductAssociation",
+
+    "Session",
 )
 
 from app1.core.models.base import Base
-from app1.core.models.user import User
-from app1.core.models.access_token import AccessToken
+from app1.core.models.users.user import User
+from app1.core.models.users.access_token import AccessToken
 
 from app1.core.models.store.rubric import Rubric, RubricImage
 from app1.core.models.store.product import Product, ProductImage
 from app1.core.models.store.brand import Brand, BrandImage
 from app1.core.models.store.rubric_product_association import RubricProductAssociation
+
+from app1.core.models.sessions.session import Session
--- app1/core/models/users/user.py
+++ app1/core/models/users/user.py
@@ -4,19 +4,26 @@ from fastapi_users.db import (
     SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase,
 )
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app1.core.models.base import Base
 from app1.core.models.mixins import IDIntPkMixin
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from app1.core.models import Session
 
 
 class User(Base, IDIntPkMixin, SQLAlchemyBaseUserTable[int]):
     display_name: Mapped[str] = mapped_column(
         default='Incognito', server_default='Noname',
         nullable=False, type_=String(50),
+    )
+
+    session: Mapped['Session'] = relationship(
+        "Session",
+        back_populates="user",
+        cascade="all, delete",
     )
 
     def __str__(self):

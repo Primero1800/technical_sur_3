f0be721400ed27a25a6877837a0a339d143bfadd
Primero1800 <Primero@inbox.ru>
2025-04-06 12:29:13 +0300

    feat(app) main temporary fragment added

--- app1/main.py
+++ app1/main.py
@@ -1,9 +1,18 @@
+from json import JSONDecodeError
+
 import pytz
 import uvicorn
 
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, BackgroundTasks, Request, Response, Body
+from fastapi.responses import ORJSONResponse
+from pydantic import Json
 from starlette import status
+from starlette.datastructures import Headers
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import PlainTextResponse
+from starlette.types import Scope, Receive, Send, ASGIApp
 
 from app1.core.config import (
     AppConfigurer,
@@ -105,6 +114,39 @@ async def test_mailer(
         "status": status.HTTP_200_OK,
         "detail": "Message has been successfully scheduled"
     }
+
+
+# TEMPORARY FRAGMENT
+
+from app1.core.sessions.views_temp import router as temp_router
+app.include_router(
+    temp_router,
+    tags=[settings.tags.TECH_TAG,]
+)
+
+
+@app.middleware("http")
+async def add_custom_header(request: Request, call_next):
+    request.headers._list.append(("x-custom-header11".encode(), "CustomHeaderValue11".encode()))
+    response = await call_next(request)
+    # response.headers["X-Custom-Header2"] = "CustomHeaderValue2"
+    return response
+
+
+@app.post("/request-response/", tags=[settings.tags.TECH_TAG,],)
+@app.post("/request-response/", tags=[settings.tags.TECH_TAG,], include_in_schema=False,)
+async def request_response(
+        request: Request,
+        response: Response,
+):
+    return {
+        "request": dict(request.headers),  # преобразуем в dict для сериализации
+        "response": {
+            "headers": dict(response.headers),  # преобразуем в dict для сериализации
+            "status_code": response.status_code,  # добавляем статус
+        },
+    }
+
 
 
 if __name__ == "__main__":
